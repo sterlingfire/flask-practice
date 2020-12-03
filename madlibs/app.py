312afe34a1eda246ddc0d05b0072a11df5f97534
J. Sterling Bishop <jsterlingbishop@gmail.com>
2020-12-02 16:08:19 -0800

    app.py

--- madlibs/app.py
+++ madlibs/app.py
@@ -7,8 +7,20 @@ app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+# silly_story = Story(
+#     ["place", "noun", "verb", "adjective", "plural_noun"],
+#     """Once upon a time, in a long-ago {place}, there lived an exceptionally
+#        {adjective} {noun}. It loved to {verb} with {plural_noun}."""
+# )
+
 
 @app.route("/")
 def view_index():
-    """Renders Index.html"""
-    return render_template(something) # Needs work
+    """Create form to generate Madlibs story"""
+    return render_template("questions.html",
+                           story=silly_story)
+
+
+@app.route("/results")
+def show_story():
+    return render_template("story.html", story=silly_story)
